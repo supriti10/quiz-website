6e0cce40bac08d8243a2c2ccbec769e5c9ab6c60
supriti10 <basaksupriti65@gmail.com>
2024-06-05 14:33:10 +0530

    new questions added

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which character is used to represent the closing of a tag in HTML?",
+        answer: "C. / ",
+        options: [
+            "A. | ",
+            "B. ! ",
+            "C. / ",
+            "D. . ",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The tags in HTML are -",
+        answer: "C. not case-sensitive",
+        options: [
+            "A. case-sensitive",
+            "B. in upper case",
+            "C. not case-sensitive",
+            "D. in lower case"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many types of CSS are there?",
+        answer: "B. 3",
+        options: [
+            "A. 2",
+            "B. 3",
+            "C. 1",
+            "D. 4"
+        ]
+    },
+    {
+        numb: 6,
+        question: "RGB color value can be from -",
+        answer: "A. 0 to 255",
+        options: [
+            "A. 0 to 255",
+            "B. 0 to 254",
+            "C. 0 to 244",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Javascript is an -",
+        answer: "A. Client-side Scripting Language",
+        options: [
+            "A. Client-side Scripting Language",
+            "B. Object-Oriented Language",
+            "C. Procedural Language",
+            "D. Server-side Scripting Language"
+        ]
+    },
+    {
+        numb: 8,
+        question: "When an operator's value is NULL, the typeof returned is-",
+        answer: "B. Undefined",
+        options: [
+            "A. Boolean",
+            "B. Undefined",
+            "C. Object",
+            "D. Integer"
+        ]
+    },
+    {
+        numb: 9,
+        question: "The 'function' and 'var' are known as",
+        answer: "D. Declaration statements",
+        options: [
+            "A. Keyword",
+            "B. Data Types",
+            "C. Prototypes",
+            "D. Declaration statements"
+        ]
+    },
+    {
+        numb: 10,
+        question: "How can a datatype be declared to be a constant type in JS?",
+        answer: "A. const",
+        options: [
+            "A. const",
+            "B. var",
+            "C. let",
+            "D. constant"
+        ]
+    },
+];
